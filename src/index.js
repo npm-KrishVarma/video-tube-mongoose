ec0d0764959ad023a1f0ad009bdc29d5a7b543e0
npm-KrishVarma <varmakrish007@gmail.com>
2023-12-28 21:14:20 +0530

    utils update ApiError, ApiResponse, asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 8001
 
 dotenv.config({ path: './env' })
 
+
 connectDB()
+    .then(()=>{
+        app.on("error",(error)=>{
+            console.log('Error:', error)
+            throw error;
+        })
+        app.listen(PORT,() => {
+            console.log(`Server is Running in PORT: ${PORT}`)
+        })
+    })
+    .catch((error)=>{
+        console.log('MongoDB Connection Failed:', error)
+    })
